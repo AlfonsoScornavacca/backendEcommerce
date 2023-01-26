373e9efa0a236757fd438c59cc1545dd7654411d
Alfonso Scornavacca <alfonso.scornavacca@raona.com>
2023-01-26 16:48:14 -0300

    InfrastructureModule created

---
+++ Business/InfraestructureModule.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstractions;
+using DataAccess.Abstractions;
+using DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public static class InfraestructureModule
+    {
+        public static IServiceCollection AddInfrastructureModule(this IServiceCollection services, IConfiguration configuration) => services
+            //Context
+            .AddDbContext<DataAccess.DataContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")),
+            ServiceLifetime.Transient,
+            ServiceLifetime.Scoped)
+            //Repositories
+            .AddTransient<IUserRepository, UserRepository>()
+            .AddTransient<IProductRepository, ProductRepository>()
+            .AddTransient<IOrderRepository, OrderRepository>()
+            //Services
+            .AddTransient<IUserService, IUserService>()
+            .AddTransient<IProductService, IProductService>()
+            .AddTransient<IOrderService, IOrderService>();
+    }
+}
