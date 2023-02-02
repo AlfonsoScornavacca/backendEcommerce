99fa29a87ed263b7a799fd7a1c6d2c90e5cb20d6
Alfonso Scornavacca <alfonso.scornavacca@raona.com>
2023-02-02 12:55:44 -0300

    AuthService and IAuthService created

---
+++ Business/Abstractions/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using Business.Models.Request;
+using Business.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstractions
+{
+    public interface IAuthService
+    {
+        Task<string> Login(LoginRequest request);
+        Task<UserResponse> Me(string email);
+
+    }
+}
